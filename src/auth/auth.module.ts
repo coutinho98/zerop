54b46107e5964b8cb00b564519d9eacad6a1a971
coutinho98 <mateuscouto98@outlook.com>
2026-01-12 19:13:18 -0300

    feat: added AuthController

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,18 +4,20 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller'; 
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-    imports: [
-        PrismaModule,
-        PassportModule,
-        JwtModule.register({
-            secret: process.env.JWT_SECRET,
-            signOptions: { expiresIn: '60m' },
-        }),
-    ],
-    providers: [AuthService, JwtStrategy],
-    exports: [AuthService, JwtModule],
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  controllers: [AuthController], 
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtModule],
 })
-export class AuthModule { }
+export class AuthModule {}
